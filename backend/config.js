9d6d38dd10de32e04406d85fd31443b28bcefc58
Santosh <santoshkalwar@gmail.com>
2023-11-24 12:42:05 +0200

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://kalwar:kalwar@fullstack.dkkpntk.mongodb.net/?retryWrites=true&w=majority';
+  'mongodb+srv://username:password@fullstack.dkkpntk.mongodb.net/?retryWrites=true&w=majority';
